9882bba5c9978b1dd1d197a6c505d09a41e19798
Anton Siabro <anton.siabro@tonicforhealth.com>
2020-04-26 22:37:54 +0300

    no message

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -6,8 +6,16 @@ public class Client {
         try (Connector connector = new Connector("127.0.0.1", 8000)){
             System.out.println("ConnectedToServer");
 
-            String request = "Kiev";
-            System.out.println("Request sent: "+ request);
+            //String request = "<?xml version = '1.0'?><class>" +
+            //        "<user email = 'asyabro@gmail.com'>" +
+            //        "<password>qwerty123</password>" +
+            //        "<action>roll_dice</action>" +
+            //        "</user></class>";
+            String request = "<?xml version = '1.0'?><class>" +
+                    "<customer id='100'><age>29</age><name>mkyong</name>" +
+                    "</customer></class>";
+
+            System.out.println("Request sent: " + request);
             connector.writeLine(request);
 
             String response = connector.readLine();
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -3,8 +3,14 @@ import java.net.ServerSocket;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import siabroPack.Connector;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 public class Server {
     public static final String ANSI_RESET = "\u001B[0m";
@@ -25,14 +31,33 @@ public class Server {
                     new Thread(() -> {
                         String request = connector.readLine();      consoleLog("Request received: " + request);
 
+
+
+
+
+
+
+
                         try {
-                            dbConnector();
+                            dbQuery("SELECT", "select * from employees");
+
+                            //dbQuery("INSERT INTO", "insert into employees " +
+                            //        "(last_name, first_name, email, department, salary)" +
+                            //        " values " +
+                            //        "('Wright', 'Eric', 'eric.wright@foo.com', 'HR', 33000.0)");
+
+                            //dbQuery("UPDATE","update employees " +
+                            //        "set email='john.doe@luv2code.com' " +
+                            //        "where last_name='Doe' and first_name='John'");
+
+                            //dbQuery("DELETE", "delete from employees " +
+                            //        "where last_name='Doe' and first_name='John'");
                         } catch (SQLException e) {
                             e.printStackTrace();
                         }
 
-                        String response = (int)(Math.random() * 30 - 10) + "";
-                        try {Thread.sleep(1000);} catch (InterruptedException e) {}
+                        String response = (int)(Math.random() * 30 - 10) + "?"; //temperature randomizer
+                        try {Thread.sleep(1000);} catch (InterruptedException e) {} //high load simulation
 
                         connector.writeLine(response);              consoleLog("Response sent: " + response);
                         try {connector.close(); } catch (IOException e) { }
@@ -45,8 +70,46 @@ public class Server {
         }
     }
 
-    public static void dbConnector () throws SQLException {
-        String dbUrl = "jdbc:mysql://localhost:3306/demo" +
+    @XmlRootElement
+    public class Customer {
+
+        String name;
+        int age;
+        int id;
+
+        public String getName() {
+            return name;
+        }
+
+        @XmlElement
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        @XmlElement
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        @XmlAttribute
+        public void setId(int id) {
+            this.id = id;
+        }
+
+    }
+
+    public static void dbQuery (String action, String query) throws SQLException {
+        String dbUrlSrv = "jdbc:mysql://localhost:3306/";
+        String dbName = "demo"; //test for Udemy
+        String dbUrl = dbUrlSrv + dbName +
                 "?characterEncoding=utf8" +
                 "&useUnicode=true" +
                 "&useJDBCCompliantTimezoneShift=true" +
@@ -56,12 +119,23 @@ public class Server {
         String pass = "student";
 
         try {
-            Connection myConn = DriverManager.getConnection(dbUrl, user, pass);
-            consoleLog(">>> DB connection successfull.");
+            Connection myConn = DriverManager.getConnection(dbUrl, user, pass); consoleLog(dbUrlSrv + dbName + " connected successfully");
             Statement myStmt = myConn.createStatement();
-            ResultSet myRs = myStmt.executeQuery("select * from employees");
-            while (myRs.next()){
-                consoleLog( myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+            if (action == "SELECT") {
+                ResultSet myRs = myStmt.executeQuery(query);
+                while (myRs.next()){
+                    consoleLog(myRs.getString("id") + ", "
+                            + myRs.getString("last_name") + ", "
+                            + myRs.getString("first_name") + ", "
+                            + myRs.getString("email") + ", "
+                            + myRs.getString("department") + ", "
+                            + myRs.getString("salary"));
+                }
+            } else if (action == "INSERT INTO" || action == "UPDATE" || action == "DELETE") {
+                int rowsAffected = myStmt.executeUpdate(query);
+                consoleLog(action + " DB action rows affected=" + Integer.toString(rowsAffected));
+            } else {
+                consoleLog(action + ": NOT IMPLEMENTED");
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -78,7 +152,7 @@ public class Server {
         }
         catch( Exception e )
         {
-            System.out.println(ANSI_GREEN + ": " + formatter.format(date) + ANSI_BLUE + e.getStackTrace()[1].getClassName() +
+            System.out.println(ANSI_GREEN + formatter.format(date) + ": " + ANSI_BLUE + e.getStackTrace()[1].getClassName() +
                     "." +
                     e.getStackTrace()[1].getMethodName() +
                     ": " + ANSI_RESET + text);
